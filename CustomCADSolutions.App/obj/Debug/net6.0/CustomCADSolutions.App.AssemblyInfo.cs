1d409815d7b09c678618c8c767b6878b4191b879
NinjataWRLD <ivanangelov414@gmail.com>
2023-12-21 22:27:02 +0200

    Add Changes

--- CustomCADSolutions.App/obj/Debug/net6.0/CustomCADSolutions.App.AssemblyInfo.cs
+++ CustomCADSolutions.App/obj/Debug/net6.0/CustomCADSolutions.App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CustomCADSolutions.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7164fa37345b80d2d72c6fc384ce553c5f361955")]
 [assembly: System.Reflection.AssemblyProductAttribute("CustomCADSolutions.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CustomCADSolutions.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
