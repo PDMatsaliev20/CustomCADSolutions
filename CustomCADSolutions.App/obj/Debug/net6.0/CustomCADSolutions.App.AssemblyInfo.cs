f5013c6cb209c9d8c3aa5c6d6983396a5e8152d8
NinjataWRLD <ivanangelov414@gmail.com>
2024-04-05 21:34:08 +0300

    Forgor Bin and Obj

--- CustomCADSolutions.App/obj/Debug/net6.0/CustomCADSolutions.App.AssemblyInfo.cs
+++ CustomCADSolutions.App/obj/Debug/net6.0/CustomCADSolutions.App.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CustomCADSolutions.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5388753c3123bc7a4b644730d68ce631c8ec0e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+836bcbfbf26e0eb455fe38c018c714e8ba36b37e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CustomCADSolutions.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CustomCADSolutions.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
