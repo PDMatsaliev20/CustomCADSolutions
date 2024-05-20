6c5ba5a8a40ac255130c71fef9ced29a62e04a87
Ivan Angelov <ivanangelov414@gmail.com>
2024-05-21 00:50:40 +0300

    Installed packages for React and tinkered a bit

--- customcadsolutions/src/main.jsx
+++ customcadsolutions/src/main.jsx
@@ -1,6 +1,7 @@
-import { createRoot } from 'react'
-import { StrictMode } from 'react-dom/client'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import 'tailwindcss/tailwind.css'
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
