fc602960cbd74b2a91b726c0c5c99a5407c9f1a4
Ivan Angelov <ivanangelov414@gmail.com>
2024-08-09 02:02:10 +0300

    Set up a minimal fully working frontend implementation

---
+++ customcads/src/private/orders/purchase/components/checkout.jsx
@@ -0,0 +1,57 @@
+import { Purchase } from '@/requests/private/payment'
+import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js'
+import { useEffect, useState } from 'react'
+
+function CheckoutForm({ id, onSubmit }) {
+    const stripe = useStripe();
+    const elements = useElements();
+    const [error, setError] = useState(null);
+    const [success, setSuccess] = useState(false);
+    const [processing, setProcessing] = useState(false);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setProcessing(true);
+
+        const card = elements.getElement(CardElement);
+
+        const { error, paymentMethod } = await stripe.createPaymentMethod({ type: 'card', card, });
+        if (error) {
+            setError(error);
+            setProccessing(false);
+            return;
+        }
+
+        const { data, status } = await Purchase(id, paymentMethod.id);
+        if (status === 404) {
+            if (data.clientSecret) {
+                const { error: confirmError } = await stripe.confirmCardPayment(data.clientSecret);
+                if (confirmError) {
+                    setError(confirmError.message);
+                    setProcessing(false);
+                    return;
+                }
+            } else {
+                setError(data);
+                setProcessing(false);
+            }
+        }
+
+        await onSubmit(paymentMethod.id);
+        setSuccess(true);
+        setProcessing(false);
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <CardElement />
+            <button type="submit" disabled={!stripe || processing}>
+                {processing ? 'Processing...' : 'Pay'}
+            </button>
+            {error && <div>{error}</div>}
+            {success && <div>Payment successful!</div>}
+        </form>
+    );
+}
+
+export default CheckoutForm;
--- customcads/src/private/orders/purchase/purchase.jsx
+++ customcads/src/private/orders/purchase/purchase.jsx
@@ -1,5 +1,51 @@
+import Spinner from '@/components/spinner'
+import CheckoutForm from './components/checkout'
+import { GetPublicKey } from '@/requests/private/payment'
+import { OrderExisting } from '@/requests/private/orders'
+import { useParams, useNavigate } from 'react-router-dom'
+import { loadStripe } from '@stripe/stripe-js'
+import { Elements } from '@stripe/react-stripe-js'
+import { useState, useEffect } from 'react'
+
 function PurchasePage() {
-    
+    const [pk, setPk] = useState();
+    const [stripePromise, setStripePromise] = useState();
+    const { id } = useParams();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        fetchPublicKey();
+    }, []);
+
+    useEffect(() => {
+        if (pk) {
+            setStripePromise(loadStripe(pk));
+        }
+    }, [pk]);
+
+    const handlePurchase = async () => {
+        try {            
+            await OrderExisting(id);
+            navigate('/orders/finished');
+        } catch (e) {
+            console.error(e);
+        }
+    };
+
+    return (
+        <>
+            {stripePromise
+                ? <Elements stripe={stripePromise}>
+                    <CheckoutForm id={id} onSubmit={handlePurchase} />
+                </Elements>
+                : <Spinner />}
+        </>
+    );
+
+    async function fetchPublicKey() {
+        const { data } = await GetPublicKey();
+        setPk(data);
+    }
 }
 
 export default PurchasePage;
